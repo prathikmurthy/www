1c0dc41a2f37c597fcfef1cdfc9578758ca72275
Prathik M <murthy.prathik@gmail.com>
2022-06-15 11:34:30 -0400

    loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,6 +2,7 @@ import {react, useEffect} from 'react'
 import ReactTypingEffect from 'react-typing-effect';
 // const anime = require('animejs');
 import anime from 'animejs/lib/anime.es.js';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Loader() {
     const animate = () => {
@@ -18,11 +19,19 @@ export default function Loader() {
             easing: 'easeInOutQuad'
         })
         loader.add({
+            targets: '#load',
+            // translateY: 100,
+            opacity: 1,
+            delay: 2200,
+            duration: 1000,
+            easing: 'easeInOutQuad'
+        })
+        loader.add({
             targets: '#tag',
             translateY: -100,
             opacity: 0,
-            delay: 3200,
-            duration: 1000,
+            delay: 300,
+            duration: 500,
             easing: 'easeInOutQuad'
         })
         // loader.add({
@@ -42,16 +51,18 @@ export default function Loader() {
 
     return (
         <div className="absolute inset-0 min-w-screen min-h-screen" id="main">
-            <div className="opacity-0 text-left pl-36" id="tag">
+            <div className="opacity-0 text-left pl-36 pt-40" id="tag">
+                <CircularProgress color="secondary" id="load" className="opacity-0"/>
                 <ReactTypingEffect
                     id="tag"
-                    className="text-5xl text-purple-500"
+                    className="pl-5 text-5xl text-purple-500"
                     text={"npm start"}
                     staticText={"C:\\PrathikM\\>"}
                     typingDelay={1700}
                     speed={100}
                     displayTextRenderer={(text, i) => <code>{text}</code>}
                 />
+                
             </div>
         </div>
     )
