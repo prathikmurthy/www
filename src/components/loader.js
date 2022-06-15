ffa36e3e10bcc6d66edea23ba8dc8534c68119f1
Prathik M <murthy.prathik@gmail.com>
2022-06-15 13:43:12 -0400

    removed circle progress from loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -18,19 +18,19 @@ export default function Loader() {
             duration: 1000,
             easing: 'easeInOutQuad'
         })
-        loader.add({
-            targets: '#load',
-            // translateY: 100,
-            opacity: 1,
-            delay: 2200,
-            duration: 1000,
-            easing: 'easeInOutQuad'
-        })
+        // loader.add({
+        //     targets: '#load',
+        //     // translateY: 100,
+        //     opacity: 1,
+        //     delay: 2200,
+        //     duration: 1000,
+        //     easing: 'easeInOutQuad'
+        // })
         loader.add({
             targets: '#tag',
             translateY: -100,
             opacity: 0,
-            delay: 300,
+            delay: 3200,
             duration: 500,
             easing: 'easeInOutQuad'
         })
@@ -43,11 +43,11 @@ export default function Loader() {
 
     return (
         <div className="absolute inset-0 min-w-screen min-h-screen" id="main">
-            <div className="opacity-0 text-left pl-4 pt-12 md:pl-48 md:pt-40 2xl:pl-96 2xl:pt-64" id="tag">
-                <CircularProgress color="secondary" id="load" className="opacity-0"/>
+            <div className="opacity-0 text-left pl-16 pt-12 md:pl-36 md:pt-40 2xl:pl-96 2xl:pt-64" id="tag">
+                {/* <CircularProgress color="secondary" id="load" className="opacity-0"/> */}
                 <ReactTypingEffect
                     id="tag"
-                    className="pl-5 l:text-5xl text-xl text-purple-500"
+                    className="pl-5 md:text-5xl text-xl text-purple-500"
                     text={"npm start"}
                     staticText={"C:\\PrathikM\\>"}
                     typingDelay={1700}
