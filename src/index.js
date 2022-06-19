bdf872abd13ee4210e991459072c087bca895319
Prathik M <murthy.prathik@gmail.com>
2022-06-18 23:50:24 -0400

    a

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import Hero from './components/sections/hero.js'
 import About from './components/sections/about.js'
 import MainProject from './components/MainProject.js'
 import Footer from './components/sections/Footer.js'
+import FadeInSection from './components/FadeInSection.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div >
+    <div className="2xl:pl-96 2xl:pt-64 md:pl-32 md:pt-40 pt-32 pr-8 pl-12">
     <Loader />
     <Hero />
+    <About />
     <Footer />
     </div>
   </React.StrictMode>
